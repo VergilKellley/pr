b4c0b84bd1335f7725a6003f848768ba970679a1
Vergil Kelley <verg_1@yahoo.com>
2018-12-20 14:32:41 -0600

    added text and image to header

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+	date_default_timezone_set('Europe/Amsterdam');
 ?>
 
 <!DOCTYPE html>
@@ -7,8 +8,13 @@
 <head>
 	<title>Puerto Rican Police Association of Chicago</title>
 
-	<!---------------main style sheet--------------------------->
+	
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<!------------------bootstrap min.css file------------------>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+	<!-------------------jquery cdn-------------------------------->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 
 	<meta charset="utf-8">
@@ -18,8 +24,49 @@
 	<meta name="keywords" content="">
 	<meta name="Vergil Kelley" content="">
 
-	<script >
+	<script src="js/main.js"></script>
 
+	<!------------JavaScript Clock ----------------------------------------->
+	<script>
+			// Comment out the PHP line you will actually use for demostration purposes
+		 var d = new Date(<?php echo time() * 1000 ?>);
+		//var d = new Date(1389971032 * 1000);
+
+		function updateClock() {
+		  // Increment the date
+		  d.setTime(d.getTime('') + 1000);
+
+		  // Translate time to pieces
+		  var currentHours = d.getHours();
+		  var currentMinutes = d.getMinutes();
+		  var currentSeconds = d.getSeconds();
+
+		  // Add the beginning zero to minutes and seconds if needed
+		  currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+		  currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+		  // Add either "AM" or "PM"
+		  var timeOfDay = (currentHours < 12) ? "am" : "pm";
+
+		  // Convert the hours our of 24-hour time
+		  currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+		  currentHours = (currentHours == 0) ? 12 : currentHours;
+
+		  // Generate the display string
+		  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
+		  // Update the time
+		  document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+		}
+
+		window.onload = function() {
+		  updateClock();
+		  setInterval('updateClock()', 1000);
+		}
+	</script>
+
+	<script >
+		/*********show more comments*****************/
 	$(document).ready(function() {
 		var commentCount = 2;
 		$('.more-commments').click(function() {
@@ -32,37 +79,91 @@
 		
 	</script>
 
+	<script>
+			/****display signup-form********/
+			$(function(){
+		   $('#signupButton').on('click',function(){  
+		      $('#signup-form').show();
+		   });
+		});
+	</script>
+
+	<script>
+		$(function(){
+		   $('#signupButton').on('click',function(){  
+		      $('#signupButton').hide();
+		   });
+		});
+	</script>
+
 </head>
 <body>
 	<header>
-		<nav>
-			<a href="#">
-				<img src="img/logo.jpg">
-			</a>
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="about.php">About</a></li>
-				<li><a href="gallery.php">Gallery</a></li>
-				<li><a href="contact.php">Contact</a></li>
-				<li><a href="donate.php">Donate</a></li>
-			</ul>
-			<div>
-				<?php
-			if (isset($_SESSION['userId'])) {
-					echo '<form action="includes/logout.inc.php" method="post">
-					<button type="submit" name="logout-submit">Logout</button>
-				</form>';
-		}
-				else {
-					echo '<form action="includes/login.inc.php" method="post">
-					<input type="text" name="mailuid" placeholder="Username/Email...">
-					<input type="password" name="pwd" placeholder="Password...">
-					<button type="submit" name="login-submit">Login</button>
-				</form>
-				<a href="signup.php">SignUp</a>';
-			}
-		?>	
-					
+	<div class="header-wrapper">
+		<div class="container-fluid date">
+			<!--<p id="datetime"></p>-->
+			<span>Chicago<div id="clock">&nbsp;</div></span>
+		</div>
+	</div>
+	
+		<div class="container-fluid logo login-signup">
+			<!--<div class="row">
+				<div class="col-sm-12 col-md-5 logo">-->
+				<div id="logo-phone">
+					<a href="#">
+						<img src="img/logo.jpg">
+					</a>
+					<p>Puerto Rican Police Association</p>
+					<p>773.888.7176</p>
+				</div>
+					<!--<?php
+						if (isset($_SESSION['userId'])) {
+							echo '<form action="includes/logout.inc.php" method="post">
+								<button type="submit" name="logout-submit">Logout</button>
+								</form>';
+							} else {
+							echo '<form action="includes/login.inc.php" method="post">
+								<input type="text" name="mailuid" placeholder="Username/Email...">
+								<input type="password" name="pwd" placeholder="Password...">
+								<button type="submit" name="login-submit">Login</button>
+								</form>
+								';
+							}
+						?>
+				</div>-->
+				<!--<div class="col-sm-12 col-md-7">-->
+					<div id="flag">
+						<img src="img/flag3kings.jpg">
+					</div>
+				<!--</div>
+			</div>-->
+		</div>
+
+
+
+		<div class="container-fluid p-0">
+			<div class="row">
+				<div class="col-sm-12">
+					<nav>
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="upcoming.php">Upcoming Events</a></li>
+							<li><a href="news.php">News</a></li>
+							<li><a href="join.php">Join PRPA</a></li>
+							<li><a href="board.php">Board Members</a></li>
+							<li><a href="partnerships.php">Partnerships</a></li>
+							<li><a href="about.php">About</a></li>
+							<li><a href="gallery.php">Gallery</a></li>
+							<li><a href="contact.php">Contact</a></li>
+							<li><a href="donate.php">Donate</a></li>
+						</ul>
+					</nav>
+				</div>
 			</div>
-		</nav>
+		</div>
 	</header>
+
+	<!--<script>
+		var date = new Date();
+		document.getElementById('datetime').innerHTML = date;
+	</script>-->
