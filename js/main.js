bddefbd01319dc247d880b3d33387aafb1d660b8
Vergil Kelley <verg_1@yahoo.com>
2018-12-22 18:38:23 -0600

    added image slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//header.php image slider
 $(document).ready(function(){
 	var imageName = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg", "img/img5.jpg", "img/img6.jpg", "img/img7.jpg", "img/img8.jpg"];
 	var imageTitle = [];
@@ -11,9 +12,11 @@ $(document).ready(function(){
 			counter++;
 			if (counter > 7) {counter = 0;}
 			$("#picture").fadeIn(500).delay(3000);
-			go();
+			go();// comment this line out to turn into click image slider
 		});
 	}
 	clickaway();
+	//uncomment below to turn into click image slider
 	//$("#picture").click(clickaway);
 });
+
